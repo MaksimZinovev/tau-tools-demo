4d164b028f1f27721a5f999223e1c5639a01c2a5
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2020-07-19 18:26:48 +1000

    update readme files

--- py-seleniumbase/README.md
+++ py-seleniumbase/README.md
@@ -1,5 +1,7 @@
-  # Video demo
-  ![video demo](gumtree.gif)
-  
-  # Requirements
-  ![requirements.txt](requirements.txt)
+ # Web UI Automated Testing Using SeleniumBase framework and Python
+
+SeleniumBase is an all-in-one framework for web automation, end-to-end testing, web presentations, and website tours. Tests are run with "pytest". Browsers are controlled by WebDriver.  
+
+![video demo](gumtree.gif)
+
+
---
+++ py-tavern-api/tests/utils 2.py
@@ -0,0 +1,83 @@
+# import requests
+import pytest
+import logging
+from random import randint
+from box import Box
+from json import dumps
+
+
+def get_id(response):
+    # Make sure that  id=1 in the response
+
+    assert response.json().get("id") == 1
+    assert response.json().get("userId") == 1
+
+def demo_extra_kwargs(response, arg1, arg2):
+    # Make sure that arg1 received
+
+    logging.info(f'\nArg1: {arg1}')
+    logging.info(f'\nArg2: {arg2}')
+
+
+
+def get_userid(response):
+    # Make sure that  id=1 in the response
+
+    assert response.json().get("userId") == 1
+
+
+def generate_req1():
+
+    req = {
+    "i": "avocado",
+    "q": "kale",
+    "p": 1,
+        }
+
+    return Box(req)
+
+
+def generate_response2():
+    joke = [
+  {
+    "id": 50,
+    "type": "general",
+    "setup": "What do you call a factory that sells passable products?",
+    "punchline": "A satisfactory"
+  }
+]
+
+    return Box(joke)
+
+
+def generate_req_rand():
+    rint = str(randint(0,1000))
+    req = {
+        "first_name": "Max",
+        "last_name": f'Smith{rint}',
+        "gender": "male",
+        "email": f'maxgsmith{rint}1982@sm.com',
+        "status": "active"
+    }
+    # logging.info(f'\ngen json {dumps(req, indent=4)}')
+    logging.info(f'\ngen Box {dumps(Box(req), indent=4)}')
+    return Box(req)
+
+
+
+
+def log_response(response):
+    # logging.info(f'\nJson response: {dumps(response.json(), indent=4)}')
+    return
+
+
+
+
+
+def save_data(response):
+    return Box({"test_title": response.json()["results"][0]["title"]})
+
+# @pytest.fixture(autouse=True)
+# def response_after_test(pytestconfig, response):
+#     # if pytestconfig.getoption('sr'):
+#     logging.info(f"Got response: {dumps(response.json(), indent=4)}")
